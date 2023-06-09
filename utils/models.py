332a589084c41cb6d70bb0b263e2a8d012855def
Mateusz <61660055+yoggys@users.noreply.github.com>
2023-06-10 00:13:52 +0200

    some formating changes

--- utils/models.py
+++ utils/models.py
@@ -8,30 +8,33 @@ from tortoise.models import Model
 class BaseModel(Model):
     class Meta:
         abstract = True
-    
+
+
 class CryptoModel(BaseModel):
     class Meta:
         indexes = ["id"]
-        
+
     id = fields.IntField(pk=True)
     tag = fields.CharField(max_length=3)
     name = fields.CharField(max_length=32)
     price = fields.FloatField()
     changes = fields.IntField(default=10)
-    
+
+
 class CryptoHistoryModel(BaseModel):
     class Meta:
         indexes = ["id", "crypto_id"]
-        
+
     id = fields.IntField(pk=True)
     crypto_id = fields.IntField()
     price = fields.FloatField()
     created_at = fields.DatetimeField(auto_now_add=True)
-    
+
+
 class EconomyModel(BaseModel):
     class Meta:
         indexes = ["id", "user_id"]
-        
+
     id = fields.IntField(pk=True)
     user_id = fields.IntField()
     balance = fields.IntField(default=float(getenv("STARTING_BALANCE")))
